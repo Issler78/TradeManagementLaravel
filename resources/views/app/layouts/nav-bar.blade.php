f09d3339ed1812e99962deb9fab15e198ffd857d
issler78 <132786398+Issler78@users.noreply.github.com>
2023-12-24 14:06:00 -0300

    create main layout + navbar

---
+++ resources/views/app/layouts/nav-bar.blade.php
@@ -0,0 +1,22 @@
+<nav class="navbar navbar-expand-md navbar-dark bg-dark">
+    <div class="container-fluid">
+        <a class="navbar-brand" href="/stores">
+            <i class="bi bi-shop"></i> My Trades
+        </a>
+
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div class="collapse navbar-collapse" id="navbarNav">
+            <div class="navbar-nav mx-auto">
+                <a class="nav-link" href="/stores">My stores</a>
+            </div>
+
+            <div class="d-flex">
+                <button class="btn btn-outline-light me-2">Log in</button>
+                <button class="btn btn-outline-light">Log out</button>
+            </div>
+        </div>
+    </div>
+</nav>
---
+++ resources/views/app/stores/stores.blade.php
@@ -0,0 +1,7 @@
+@extends('app.layouts.main')
+
+@section('title', 'My Stores')
+
+@section('content')
+<h1>Test</h1>
+@endsection
