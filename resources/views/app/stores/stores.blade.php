cb63c4ee6de0b3262d4fa86b4cdf749c373a1821
issler78 <132786398+Issler78@users.noreply.github.com>
2023-12-25 00:38:32 -0300

    main font + fix index view

--- resources/views/app/stores/stores.blade.php
+++ resources/views/app/stores/stores.blade.php
@@ -3,29 +3,37 @@
 @section('title', 'My Stores')
 
 @section('content')
-<div class="d-flex justify-content">
-    <table class="table table-dark table-hover table-striped my-3 mx-3">
-        <thead>
-            <tr>
-                <th scope="col">Store</th>
-                <th scope="col">Postal Code</th>
-                <th scope="col">Status</th>
-                <th scope="col">Actions</th>
-            </tr>
-        </thead>
-        <tbody class="table-group-divider">
-            @foreach ($stores as $store)                
+<div class="py-3 px-3">
+    <div class="d-flex justify-content-between">
+        <h1>Stores</h1>
+
+        <button class="btn btn-outline-dark mb-3 d-flex justify-content-between align-self-end gap-3" title="Add Store">Add Store <i class="bi bi-plus-lg"></i></button>
+    </div>
+
+    <div class="d-flex justify-content">
+        <table class="table table-dark table-hover table-striped">
+            <thead>
                 <tr>
-                    <td>{{ $store['store_name'] }}</td>
-                    <td>{{ $store['postal_code'] }}</td>
-                    <td>{{ $store['status'] }}</td>
-                    <td>
-                        <button class="btn btn-outline-light" title="Edit Store"><i class="bi bi-pencil"></i></button>
-                        <button class="btn btn-outline-light" title="View Store"><i class="bi bi-eye"></i></button>
-                    </td>
+                    <th scope="col">Store</th>
+                    <th scope="col">Postal Code</th>
+                    <th scope="col">Status</th>
+                    <th scope="col">Actions</th>
                 </tr>
-            @endforeach
-        </tbody>
-    </table>
+            </thead>
+            <tbody class="table-group-divider">
+                @foreach ($stores as $store)                
+                    <tr>
+                        <td>{{ $store['store_name'] }}</td>
+                        <td>{{ $store['postal_code'] }}</td>
+                        <td>{{ $store['status'] }}</td>
+                        <td>
+                            <button class="btn btn-outline-light" title="Edit Store"><i class="bi bi-pencil"></i></button>
+                            <button class="btn btn-outline-light" title="View Store"><i class="bi bi-eye"></i></button>
+                        </td>
+                    </tr>
+                @endforeach
+            </tbody>
+        </table>
+    </div>
 </div>
 @endsection
